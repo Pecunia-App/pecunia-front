b85e3149d8c99175dc28f7fca8ae563653f65591
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2025-06-11 14:32:58 +0200

    feat(fondation-scss): task 150 - implement basic scss fondation + doc
    feat(tokens): add style-dictionary, implement script to create scss variables and add documentation
    
    feat(tokens): in json file change type to number to avoid manual task during token import
    
    number instead of dimension allow more flexibility when we use scss variables
    
    feat(fondation-scss): task 150 - implement basic scss fondation and start documentation
    
    feat(fondation-scss): task 150 - create theme service and btn switch example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ThemeSwitchComponent } from './shared/theme-switch/theme-switch.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ThemeSwitchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
