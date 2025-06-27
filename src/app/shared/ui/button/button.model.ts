3372390b67d48cf25f24cea347452aaebe97d0d6
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2025-06-27 16:59:01 +0200

    feat(fondation-css): task 163: create button UI component

---
+++ src/app/shared/ui/button/button.model.ts
@@ -0,0 +1,15 @@
+export type VariantType =
+  | 'primary'
+  | 'secondary'
+  | 'primary-ghost'
+  | 'secondary-ghost'
+  | 'alert'
+  | 'success';
+
+export type ButtonType = 'button' | 'submit' | 'reset';
+
+export type ButtonSize = 'square' | 'small' | 'medium' | 'large';
+
+export type ButtonRadius = 'medium' | 'large';
+
+export type ButtonWidth = 'full' | 'auto';
