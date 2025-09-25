8d7ecb7216f4385d6722962ff9df4ecba8fb662e
Lunardo <thomaslunardo@gmail.com>
2025-09-25 12:28:35 +0200

    feat(add-radiogroup-component): add radio component and it style

--- src/app/shared/forms/wallet-form/wallet-form.component.ts
+++ src/app/shared/forms/wallet-form/wallet-form.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ButtonComponent } from '../../ui/button/button.component';
 import { InputComponent } from '../../ui/input/input.component';
+import { RadioGroupComponent } from '../../ui/radio-group/radio-group.component';
+import { RadioOption } from '../../../_core/models/radio-option.model';
 
 @Component({
   selector: 'app-wallet-form',
-  imports: [FormsModule, ButtonComponent, InputComponent],
+  imports: [FormsModule, ButtonComponent, InputComponent, RadioGroupComponent],
   templateUrl: './wallet-form.component.html',
   styleUrl: './wallet-form.component.scss',
 })
@@ -13,8 +15,13 @@ export class WalletFormComponent {
   newWallet = {
     name: '',
     balance: 0,
-    currency: '',
+    currency: 'EUR',
   };
+
+  currencyOptions: RadioOption<string>[] = [
+    { label: 'EUR', value: 'EUR' },
+    { label: 'USD', value: 'USD' },
+  ];
 
   submitted = false;
 
