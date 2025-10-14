08d1435a0450505d319ff354828bf4661ffd5266
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2025-10-14 22:09:18 +0200

    feat(transactions): implement resolver and refactor components using one transaction

--- src/app/pages/transactions/transactions-update/transactions-update.component.ts
+++ src/app/pages/transactions/transactions-update/transactions-update.component.ts
@@ -1,8 +1,5 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ConnectedLayoutComponent } from '../../../shared/layout/connected-layout/connected-layout.component';
-import { ActivatedRoute } from '@angular/router';
-import { TransactionsService } from '../../../_core/services/transactions/transactions.service';
-import { TransactionDTO } from '../../../_core/models/transactions/transaction.dto';
 import { TransactionStore } from '../../../_core/store/transactions.store.service';
 
 @Component({
@@ -11,26 +8,8 @@ import { TransactionStore } from '../../../_core/store/transactions.store.servic
   templateUrl: './transactions-update.component.html',
   styleUrl: './transactions-update.component.scss',
 })
-export class TransactionsUpdateComponent implements OnInit {
-  private readonly route = inject(ActivatedRoute);
-  private readonly transactionsService = inject(TransactionsService);
-  private readonly transctionStore = inject(TransactionStore);
-  readonly transaction = signal<TransactionDTO | null>(null);
-  readonly loading = this.transctionStore.isLoading;
-
-  ngOnInit(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.transactionsService.getTransactionById(id).subscribe({
-      next: (tx) => {
-        this.transaction.set(tx ?? null);
-        console.log('transaction', this.transaction());
-
-        this.loading.set(false);
-      },
-      error: () => {
-        this.loading.set(false);
-        this.transaction.set(null);
-      },
-    });
-  }
+export class TransactionsUpdateComponent {
+  private readonly transactionStore = inject(TransactionStore);
+  readonly transaction = this.transactionStore.selectedTransaction;
+  readonly loading = this.transactionStore.isLoading;
 }
