5a415a875f3e152287a022c73035000e7b93c50c
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2025-10-21 16:39:20 +0200

    feat(transactions): add form in update page and implement data transaction loading

--- src/app/pages/transactions/transactions-update/transactions-update.component.ts
+++ src/app/pages/transactions/transactions-update/transactions-update.component.ts
@@ -1,15 +1,39 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { ConnectedLayoutComponent } from '../../../shared/layout/connected-layout/connected-layout.component';
 import { TransactionStore } from '../../../_core/store/transactions.store.service';
+import { TransactionFormComponent } from '../../../shared/forms/transaction-form/transaction-form.component';
+import { TransactionDTO } from '../../../_core/models/transactions/transaction.dto';
+import { formatDateFr } from '../../../_core/utils/format.utils';
+import { ButtonComponent } from '../../../shared/ui/button/button.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-transactions-update',
-  imports: [ConnectedLayoutComponent],
+  imports: [
+    ConnectedLayoutComponent,
+    TransactionFormComponent,
+    ButtonComponent,
+  ],
   templateUrl: './transactions-update.component.html',
   styleUrl: './transactions-update.component.scss',
 })
-export class TransactionsUpdateComponent {
+export class TransactionsUpdateComponent implements OnInit {
   private readonly transactionStore = inject(TransactionStore);
-  readonly transaction = this.transactionStore.selectedTransaction;
+  readonly router = inject(Router);
+  readonly transaction = signal<TransactionDTO | undefined | null>(undefined);
   readonly loading = this.transactionStore.isLoading;
+
+  ngOnInit(): void {
+    const selectedTransaction = this.transactionStore.selectedTransaction();
+    console.log(selectedTransaction);
+    this.transaction.set(selectedTransaction);
+  }
+
+  formatDate(date: string) {
+    return formatDateFr(date);
+  }
+
+  navigateToTranstaction() {
+    this.router.navigate(['/transactions/', this.transaction()?.id]);
+  }
 }
