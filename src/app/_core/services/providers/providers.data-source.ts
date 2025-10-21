d3b60c8f26eeb86cd6345388738b34fc928c04f5
txrigxn <mysubscriptions@tuta.com>
2025-10-21 15:09:52 +0200

    feat(tag-provider-create): create in parameters page are working

--- src/app/_core/services/providers/providers.data-source.ts
+++ src/app/_core/services/providers/providers.data-source.ts
@@ -1,7 +1,11 @@
 import { Observable } from 'rxjs';
-import { ProviderDTO } from '../../models/transactions/provider.dto';
+import {
+  ProviderCreateDTO,
+  ProviderDTO,
+} from '../../models/transactions/provider.dto';
 
 export interface ProvidersDataSource {
   getUserProviders(userId: number): Observable<ProviderDTO[]>;
   getProviderById(id: number): Observable<ProviderDTO>;
+  createProvider(dto: ProviderCreateDTO): Observable<ProviderDTO>;
 }
