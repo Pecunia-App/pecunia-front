01e8861b62f94d135d7feacc5da3682135c96565
AlexiaGu <150676364+AlexiaGu@users.noreply.github.com>
2025-10-23 09:50:44 +0200

    fix(user.service,-profile-form): add possibility to change profil picture

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ProfileFormComponent } from '../../shared/forms/profile-form/profile-form.component';
+import { ConnectedLayoutComponent } from '../../shared/layout/connected-layout/connected-layout.component';
 
 @Component({
   selector: 'app-profile',
-  imports: [ProfileFormComponent],
+  imports: [ProfileFormComponent, ConnectedLayoutComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
